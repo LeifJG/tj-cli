324aecfadf964d07a8e207664a617ac156f6cb8a
李健高 <lijg@yunjiglobal.com>
2022-04-02 20:03:43 +0800

    ejs 项目模板逻辑

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,8 @@
 const figlet =require('figlet')
+const fs = require('fs')
+const path = require('path')
 const chalk = require('chalk') 
+
 module.exports = {
   // 使用 figlet 绘制 Logo
   printLogo () {
@@ -23,5 +26,25 @@ module.exports = {
     console.log(chalk.green('H5'))
     console.log(chalk.green('node'))
     console.log(chalk.green('nuxt'))
+  },
+  // 查找目标目录内的指定文件
+  findFiles(filePath, fileName) { // 用于查找目标路径内的所有 .tpl 文件
+    const tplList = []
+    const findTplPath = (filePath) => {
+      const target = eval(`/${fileName}/`)
+      // const tplRegExp= /^.*\.(tpl)$/
+      const files = fs.readdirSync(filePath)
+      files.forEach((item) => {
+        const fPath = path.join(filePath, item)
+        const stat = fs.statSync(fPath)
+        if (stat.isFile() && target.test(fPath)) {
+          tplList.push(fPath)
+        } else if (stat.isDirectory() === true && fPath.indexOf(`${this.slash}node_modules`) === -1 && fPath.indexOf(`${this.slash}.git`) === -1) {
+          findTplPath(fPath)
+        }
+      })
+    }
+    findTplPath(filePath)
+    return tplList
   }
 }
